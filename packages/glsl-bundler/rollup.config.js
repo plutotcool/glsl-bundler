681f15016e254e1c77aa3da80c269b29f1fa73c0
Julien Dargelos <juliendargelos@users.noreply.github.com>
2023-05-31 10:13:23 +0200

    style(config): use single quotes

--- packages/glsl-bundler/rollup.config.js
+++ packages/glsl-bundler/rollup.config.js
@@ -9,24 +9,24 @@ import pkg from './package.json'
 const dir = path.dirname(pkg.main)
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: [
     {
-      format: "cjs",
-      entryFileNames: "[name].cjs",
+      format: 'cjs',
+      entryFileNames: '[name].cjs',
       dir,
       preserveModules: true,
     },
     {
-      format: "es",
-      entryFileNames: "[name].mjs",
+      format: 'es',
+      entryFileNames: '[name].mjs',
       dir,
       preserveModules: true,
     },
     {
-      format: "umd",
+      format: 'umd',
       file: `${dir}/index.umd.cjs`,
-      name: "GLSLBundler",
+      name: 'GLSLBundler',
     },
   ],
   plugins: [cleaner({ targets: [dir] }), autoExternal(), eslint(), ts()],
